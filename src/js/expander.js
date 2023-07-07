7500cf4c7fb30a348bfed4306774bc7469d244b9
Alec Rust <me@alecrust.com>
2023-07-07 15:44:14 +0100

    Improve consistency of JS modules, add comments

--- src/js/expander.js
+++ src/js/expander.js
@@ -1,11 +1,22 @@
+/**
+ * Attaches event listeners to elements with the class '.js-expander-button'
+ * for toggling the expansion of the associated content.
+ */
 document.querySelectorAll('.js-expander-button').forEach((button) => {
-  button.addEventListener('click', () => {
-    const expander = button.parentElement
-    const content = expander.querySelector('.Expander-content')
-    const isExpanded = expander.classList.toggle('is-expanded')
-
-    content.style.maxHeight = isExpanded ? `${content.scrollHeight}px` : ''
-    button.textContent = isExpanded ? 'Show less' : 'Show more'
-    button.setAttribute('aria-expanded', isExpanded)
-  })
+  button.addEventListener('click', () => toggleExpansion(button))
 })
+
+/**
+ * Toggles the expansion state of content associated with the given button.
+ *
+ * @param {HTMLElement} button - The button used to toggle content expansion.
+ */
+function toggleExpansion(button) {
+  const expanderContainer = button.parentElement
+  const content = expanderContainer?.querySelector('.Expander-content')
+  const isExpanded = expanderContainer.classList.toggle('is-expanded')
+
+  content.style.maxHeight = isExpanded ? `${content.scrollHeight}px` : ''
+  button.textContent = isExpanded ? 'Show less' : 'Show more'
+  button.setAttribute('aria-expanded', isExpanded)
+}
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,17 @@
 import PhotoSwipeLightbox from 'photoswipe/lightbox'
 
-const gallery = new PhotoSwipeLightbox({
-  gallery: '.js-gallery',
-  children: 'li',
-  pswpModule: () => import('photoswipe'),
-})
-gallery.init()
+/**
+ * Initializes PhotoSwipe gallery
+ */
+
+function initializeGallery() {
+  const gallery = new PhotoSwipeLightbox({
+    gallery: '.js-gallery',
+    children: 'li',
+    pswpModule: () => import('photoswipe'),
+  })
+
+  gallery.init()
+}
+
+initializeGallery()
